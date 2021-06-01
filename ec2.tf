0ca144af0e7453d8474827f2115fc8ba02e7e754
yirez <irezyigit@gmail.com>
2021-06-01 22:00:09 +0300

    fixed sec group

--- ec2.tf
+++ ec2.tf
@@ -1,116 +1,47 @@
 resource "aws_instance" "test_cluster_m1" {
-  ami           = data.aws_ami.base_image.id
-  instance_type = "t2.small"
-  key_name="terraform"
+  ami                    = data.aws_ami.base_image.id
+  instance_type          = "t2.small"
+  key_name               = "terraformec2"
   vpc_security_group_ids = [aws_security_group.sec_group_block.id]
   tags = {
     Name = "test_cluster_m1"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "echo ${aws_eip.eip_m1.public_ip}",
-      "echo ${aws_instance.test_cluster_m1.public_ip}"
-    ]
-
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("./terraform.pem")
-    }
-  }
 }
-
-
-
-
-
+ 
 resource "aws_instance" "test_cluster_m2" {
-  ami           = data.aws_ami.base_image.id
-  instance_type = "t2.small"
-  key_name="terraform"
+  ami                    = data.aws_ami.base_image.id
+  instance_type          = "t2.small"
+  key_name               = "terraformec2"
   vpc_security_group_ids = [aws_security_group.sec_group_block.id]
   tags = {
     Name = "test_cluster_m2"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "echo ${aws_eip.eip_m2.public_ip}",
-      "echo ${aws_instance.test_cluster_m2.public_ip}"
-    ]
-
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("./terraform.pem")
-    }
-  }
 }
 resource "aws_instance" "test_cluster_w1" {
-  ami           = data.aws_ami.base_image.id
-  instance_type = "t2.small"
-  key_name="terraform"
+  ami                    = data.aws_ami.base_image.id
+  instance_type          = "t2.small"
+  key_name               = "terraformec2"
   vpc_security_group_ids = [aws_security_group.sec_group_block.id]
   tags = {
     Name = "test_cluster_w1"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "echo ${aws_eip.eip_w1.public_ip}",
-      "echo ${aws_instance.test_cluster_w1.public_ip}"
-    ]
-
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("./terraform.pem")
-    }
-  }
 }
 resource "aws_instance" "test_cluster_w2" {
-  ami           = data.aws_ami.base_image.id
-  instance_type = "t2.small"
-  key_name="terraform"
+  ami                    = data.aws_ami.base_image.id
+  instance_type          = "t2.small"
+  key_name               = "terraformec2"
   vpc_security_group_ids = [aws_security_group.sec_group_block.id]
   tags = {
     Name = "test_cluster_w2"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "echo ${aws_eip.eip_w2.public_ip}",
-      "echo ${aws_instance.test_cluster_w2.public_ip}"
-    ]
-
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("./terraform.pem")
-    }
-  }
 }
 resource "aws_instance" "test_cluster_w3" {
-  ami           = data.aws_ami.base_image.id
-  instance_type = "t2.small"
-  key_name="terraform"
+  ami                    = data.aws_ami.base_image.id
+  instance_type          = "t2.small"
+  key_name               = "terraformec2"
   vpc_security_group_ids = [aws_security_group.sec_group_block.id]
   tags = {
     Name = "test_cluster_w3"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "echo ${aws_eip.eip_w3.public_ip}",
-      "echo ${aws_instance.test_cluster_w3.public_ip}"
-    ]
-
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("./terraform.pem")
-    }
   }
 }
 
--- sec_group.tf
+++ sec_group.tf
@@ -1,10 +1,12 @@
 resource "aws_security_group" "sec_group_block" {
-  name        = "custom_sec_group"
+  name = "custom_sec_group"
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1" 
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
