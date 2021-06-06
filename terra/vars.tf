b1d8f96d32a86dc5b71f13b23ab5848590fd6d7f
yirez <irezyigit@gmail.com>
2021-06-06 18:20:50 +0300

    removed unnecessary vars

--- terra/vars.tf
+++ terra/vars.tf
@@ -1,15 +1,3 @@
-
-variable "iam_name" {
-  default = "test_user"
-  type    = string
-}
-
-variable "instance_name" {
-  default = "this_is_prod"
-  type    = string
-}
-
-
 variable "datasource_ami_owner" {
   default = "amazon"
   type    = string
